26413251f347c358d8f5f359d5d886763a583fdb
Boris Aelen <boris@borisaelen.nl>
2021-03-10 22:06:22 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "vsphere_vm_portgroup" {
 variable "vsphere_vm_cpu" {
   type = number
   description = "the number of vCpus"
-  default = 2
+  default = 1
 }
 
 variable "vsphere_vm_memory" {
